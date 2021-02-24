97d0dd2d36a60d9e965be024fab2e50e7903dcb1
MrSullivan <rsullivan@stcadocs.com>
2021-02-24 23:02:33 +0000

    two input integer. print variables as averages.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
+    first_number = int(input("Give the first number:"))
+    second_number = int(input("Give the second number:"))
+    print("The average is " + str((first_number+second_number)/2))
 
 if __name__ == '__main__':
     main()
